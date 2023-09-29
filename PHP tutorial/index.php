8bccf08d32113236868459b5c66fbb8b6e173af3
janaledencan <janaledencan@gmail.com>
2023-09-29 15:35:23 +0200

    Echo and variables

---
+++ PHP tutorial/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo 'Hello World';
+echo 'Hello', ' ','World';
+echo "Joe 's Invoice";
+
+$name='Jana';
+
+echo $name;
+
+
+$x=1;
+$y=&$x;
+$x=3;
+echo $y;
+
+$firstName='Jana';
+echo "Hello $firstName";
+echo "Hello {$firstName}";
+echo 'Hello' . $firstName;
+ 
