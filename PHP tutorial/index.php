792d5958e6e4035f9477aac00ff0ab9ccbe8c2d3
janaledencan <janaledencan@gmail.com>
2023-10-05 11:23:17 +0200

    PHP Data Types

--- PHP tutorial/index.php
+++ PHP tutorial/index.php
@@ -1,20 +1,47 @@
-<!DOCTYPE html> 
-<html>
-    <body>
-        <h1>
-            <?php
-                $x=10;
-                $y=5;
-                echo $x . ', ' . $y;
-                //Comment 1
-                # Comment 2
-                /*
-                    multi line
-                */
-            ?>    
-            <?php echo 'Hello World' ?> <!--skraceno ?=-->
-        </h1>
-        <p>My first paragraph.</p>
+<?php
+/* Data Types */
 
-    </body>
-</html>
+#Scalar Types
+#bool
+$completed = true;
+
+#int
+$score = 75;
+
+#float
+$price = 0.99;
+
+#string
+$greeting = 'Hello J';
+
+echo $completed.'<br/>';
+echo $price;
+echo $greeting.'<br/>';
+
+echo gettype($price).'<br/>';
+var_dump($completed);
+
+
+#array
+$companies = [1,2,3,0.5,true];
+#echo $companies;
+print_r($companies);
+
+
+#declare(strict_types=1);
+
+function sum(int $x, int $y){
+    
+    var_dump($x,$y);
+    echo '<br />';
+    return $x + $y;
+}
+
+$sum = sum(2.5, '3');
+echo $sum . '<br />';
+
+var_dump($sum);
+
+
+$x = (int)'5';
+echo $x;
